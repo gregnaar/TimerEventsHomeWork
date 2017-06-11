f916fcf1ad6b273b0bb5d31fd04b592eaa0c2ed4
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-06-11 18:08:01 +0200

    Excercise 4 completed

--- TimerEvents/Program.cs
+++ TimerEvents/Program.cs
@@ -16,7 +16,7 @@ namespace TimerEvents
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Timer());
         }
     }
 }
--- TimerEvents/Timer.Designer.cs
+++ TimerEvents/Timer.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TimerEvents
 {
-    partial class Form1
+    partial class Timer
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(250, 41);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Timer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 74);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Timer";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.Timer_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
